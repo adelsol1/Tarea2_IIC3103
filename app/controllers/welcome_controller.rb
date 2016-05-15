95191ea6a3ed4865c42bb79799d69133a519dcbd
Alicia del Sol <adelsol1@uc.cl>
2016-05-15 16:02:26 -0300

    Prueba primer api Instagram, total con un tag

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,22 @@
+#Base del proyecto hecha con e integración con travis hecha con:
+# http://theblog.unpixel.fr/2015-12-09-continuous-delivery-with-travis-and-heroku/
+
+
 class WelcomeController < ApplicationController
+
+  def buscar
+    tag = params[:tag]
+    access_token = params[:access_token]
+    tagresponse = HHTParty.get(https://api.instagram.com/v1/tags/{tag}?access_token={access_token})
+    count = tagresponse["data"]["media_count"]
+    dataresponse = HTTParty.get(“https://api.instagram.com/v1/tags/{tag}/media/recent?access_token={access_token})
+    data = dataresponse["data"]
+    response = {:total => count}
+  end
+
+
+#obligar que devuelva status
+#render JSON:{{json}}, status: 200
+#if paramestros no estan bien 400
+
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+#require_test_helper
+#class WelcomeControllerTest
+#definir clas estilo test
+  #test do
+  #si me entrega (assert equal status, 200) post nombre metodo testeando
+  #bundle exec rake (para correrlo y ver los test si sale punto es que paso, s fallo, r error)
+  #end
+  #enunciado: coveralls-> ve que % del codigo esta sobre test
+  #readme 1 parametro es la version: 1.0.0
   #test "the truth" do
   #  assert false
   #end
